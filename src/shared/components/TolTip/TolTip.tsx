be7aac1850db63786b7e1aac0189cbc15056c88a
Ірина Дорошенко <i.k.doroshenko.2014@gmail.com>
2024-03-09 18:34:09 +0000

    Merged in 4_TolTip-component (pull request #5)
    create TolTip component

---
+++ src/shared/components/TolTip/TolTip.tsx
@@ -0,0 +1,22 @@
+import { FC, PropsWithChildren } from 'react';
+import styles from './TolTip.module.scss';
+
+interface TolTipProps {
+  message: string;
+  hidden?: boolean;
+}
+
+export const TolTip: FC<PropsWithChildren<TolTipProps>> = ({
+  message,
+  children,
+  hidden = false,
+}) => {
+  return hidden ? (
+    <>{children}</>
+  ) : (
+    <div className={styles.tooltip}>
+      <div className={styles.targetElement}>{children}</div>
+      <span className={styles.tooltipText}>{message}</span>
+    </div>
+  );
+};
---
+++ src/shared/components/TolTip/index.ts
@@ -0,0 +1,1 @@
+export { TolTip } from './TolTip';
