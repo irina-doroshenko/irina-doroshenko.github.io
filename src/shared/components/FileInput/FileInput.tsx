f93a1a21022fee076185b9e6c98f25a77b762b72
Ірина Дорошенко <i.k.doroshenko.2014@gmail.com>
2024-03-12 14:59:52 +0000

    Merged in 13_image-input (pull request #13)
    create image input field, expand plain input with additional logic for entering phone numbers, fix style bug with user cards

---
+++ src/shared/components/FileInput/FileInput.tsx
@@ -0,0 +1,50 @@
+import { FC, InputHTMLAttributes } from 'react';
+import styles from './FileInput.module.scss';
+
+interface FileInputProps
+  extends Omit<InputHTMLAttributes<HTMLInputElement>, 'type' | 'value'> {
+  id: string;
+  value: File | null;
+  buttonText: string;
+  touched?: boolean;
+  isError?: boolean;
+  errorText?: string;
+}
+
+export const FileInput: FC<FileInputProps> = ({
+  id,
+  name,
+  value,
+  onChange,
+  accept,
+  errorText,
+  isError,
+  buttonText,
+  placeholder = '',
+}) => {
+  return (
+    <div className={styles.inputWrapper}>
+      <label
+        htmlFor={id}
+        className={`${styles.input} ${isError ? styles.error : ''}`}
+      >
+        <div className={styles.button}>{buttonText}</div>
+        <input
+          id={id}
+          type="file"
+          name={name}
+          accept={accept}
+          onChange={(e) => {
+            onChange && onChange(e);
+          }}
+        />
+        <p className={value ? styles.fileName : styles.placeholder}>
+          {value ? value.name : placeholder}
+        </p>
+      </label>
+      {isError && errorText && (
+        <div className={styles.errorText}>{errorText}</div>
+      )}
+    </div>
+  );
+};
--- src/shared/components/InputField/InputField.tsx
+++ src/shared/components/InputField/InputField.tsx
@@ -1,18 +1,19 @@
-import { ChangeEvent, FC } from 'react';
+import { ChangeEvent, FC, FocusEvent } from 'react';
 import styles from './InputField.module.scss';
 
 interface InputFieldProps {
   value: string;
   name: string;
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
-  onBlur?: (e: FocusEvent) => void;
-  type?: 'text' | 'tel';
+  onBlur?: (e: FocusEvent<HTMLInputElement>) => void;
+  type?: 'text' | 'tel' | 'email';
   label: string;
   helperText?: string;
   className?: string;
   touched?: boolean;
   isError?: boolean;
   errorText?: string;
+  countyCode?: string;
 }
 
 export const InputField: FC<InputFieldProps> = ({
@@ -24,7 +25,25 @@ export const InputField: FC<InputFieldProps> = ({
   helperText,
   isError,
   errorText,
+  countyCode = '+38',
 }) => {
+  const onFocusHandler = (e: FocusEvent<HTMLInputElement>) => {
+    e.target.placeholder = '';
+    if (type === 'tel' && !e.target.value) {
+      e.target.value = countyCode;
+      onChange(e);
+    }
+  };
+
+  const onBlurHandler = (e: FocusEvent<HTMLInputElement>) => {
+    if (type === 'tel') {
+      const enteredValue = e.target.value.slice(countyCode.length);
+      !enteredValue && (e.target.value = '');
+      !enteredValue && onChange(e);
+    }
+    e.target.placeholder = label;
+  };
+
   return (
     <div className={styles.inputWrapper}>
       <input
@@ -33,12 +52,14 @@ export const InputField: FC<InputFieldProps> = ({
         onChange={onChange}
         type={type}
         placeholder={label}
-        onFocus={(e) => (e.target.placeholder = '')}
-        onBlur={(e) => (e.target.placeholder = label)}
+        onFocus={onFocusHandler}
+        onBlur={onBlurHandler}
         className={`${styles.input} ${isError && styles.error}`}
         autoComplete="off"
       />
-      <div className={`${styles.labelText} ${isError && styles.errorText}`}>
+      <div
+        className={`${styles.labelText} ${isError ? styles.errorText : ''} ${value && styles.upperText}`}
+      >
         {label}
       </div>
       {helperText && !isError && (
